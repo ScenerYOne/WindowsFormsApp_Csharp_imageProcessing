582483e966e4f64b2e523a14fdbb0acc2f6d711c
new <krittaporn485@gmail.com>
2024-11-28 14:57:57 +0700

    Add project files.

---
+++ WindowsFormsApp12/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Digital_Image_Processing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.outputBox = new System.Windows.Forms.PictureBox();
+            this.difBox = new System.Windows.Forms.PictureBox();
+            this.histogram1 = new System.Windows.Forms.PictureBox();
+            this.histogram2 = new System.Windows.Forms.PictureBox();
+            this.openImage = new System.Windows.Forms.Button();
+            this.Threshold = new System.Windows.Forms.Button();
+            this.imageQuan = new System.Windows.Forms.Button();
+            this.histogram = new System.Windows.Forms.Button();
+            this.imageDifference = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outputBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.difBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.histogram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.histogram2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(103, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(361, 217);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(574, 39);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(413, 217);
+            this.outputBox.TabIndex = 1;
+            this.outputBox.TabStop = false;
+
+            // 
+            // difBox
+            // 
+            this.difBox.Location = new System.Drawing.Point(1075, 39);
+            this.difBox.Name = "difBox";
+            this.difBox.Size = new System.Drawing.Size(379, 217);
+            this.difBox.TabIndex = 2;
+            this.difBox.TabStop = false;
+            // 
+            // histogram1
+            // 
+            this.histogram1.Location = new System.Drawing.Point(84, 333);
+            this.histogram1.Name = "histogram1";
+            this.histogram1.Size = new System.Drawing.Size(625, 263);
+            this.histogram1.TabIndex = 3;
+            this.histogram1.TabStop = false;
+            // 
+            // histogram2
+            // 
+            this.histogram2.Location = new System.Drawing.Point(862, 333);
+            this.histogram2.Name = "histogram2";
+            this.histogram2.Size = new System.Drawing.Size(592, 263);
+            this.histogram2.TabIndex = 4;
+            this.histogram2.TabStop = false;
+            // 
+            // openImage
+            // 
+            this.openImage.Location = new System.Drawing.Point(40, 687);
+            this.openImage.Name = "openImage";
+            this.openImage.Size = new System.Drawing.Size(189, 63);
+            this.openImage.TabIndex = 5;
+            this.openImage.Text = "Open Button";
+            this.openImage.UseVisualStyleBackColor = true;
+            this.openImage.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Threshold
+            // 
+            this.Threshold.Location = new System.Drawing.Point(331, 687);
+            this.Threshold.Name = "Threshold";
+            this.Threshold.Size = new System.Drawing.Size(202, 63);
+            this.Threshold.TabIndex = 6;
+            this.Threshold.Text = "Threshold";
+            this.Threshold.UseVisualStyleBackColor = true;
+            this.Threshold.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // imageQuan
+            // 
+            this.imageQuan.Location = new System.Drawing.Point(637, 673);
+            this.imageQuan.Name = "imageQuan";
+            this.imageQuan.Size = new System.Drawing.Size(192, 91);
+            this.imageQuan.TabIndex = 7;
+            this.imageQuan.Text = "Image\r\nQuantization";
+            this.imageQuan.UseVisualStyleBackColor = true;
+            this.imageQuan.Click += new System.EventHandler(this.imageQuan_Click);
+            // 
+            // histogram
+            // 
+            this.histogram.Location = new System.Drawing.Point(959, 673);
+            this.histogram.Name = "histogram";
+            this.histogram.Size = new System.Drawing.Size(192, 91);
+            this.histogram.TabIndex = 8;
+            this.histogram.Text = "Histogram \r\nEqualization\r\n";
+            this.histogram.UseVisualStyleBackColor = true;
+            this.histogram.Click += new System.EventHandler(this.histogram_Click);
+            // 
+            // imageDifference
+            // 
+            this.imageDifference.Location = new System.Drawing.Point(1259, 701);
+            this.imageDifference.Name = "imageDifference";
+            this.imageDifference.Size = new System.Drawing.Size(243, 63);
+            this.imageDifference.TabIndex = 9;
+            this.imageDifference.Text = "Image Difference";
+            this.imageDifference.UseVisualStyleBackColor = true;
+            this.imageDifference.Click += new System.EventHandler(this.imageDifference_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(158, 268);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 36);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Input Image";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(693, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(240, 45);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Output Image";
+
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1153, 268);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(214, 36);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Different Image";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(277, 611);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(224, 36);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Input Histogram";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1025, 611);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(246, 36);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Output Histogram";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1514, 841);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imageDifference);
+            this.Controls.Add(this.histogram);
+            this.Controls.Add(this.imageQuan);
+            this.Controls.Add(this.Threshold);
+            this.Controls.Add(this.openImage);
+            this.Controls.Add(this.histogram2);
+            this.Controls.Add(this.histogram1);
+            this.Controls.Add(this.difBox);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outputBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.difBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.histogram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.histogram2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox outputBox;
+        private System.Windows.Forms.PictureBox difBox;
+        private System.Windows.Forms.PictureBox histogram1;
+        private System.Windows.Forms.PictureBox histogram2;
+        private System.Windows.Forms.Button openImage;
+        private System.Windows.Forms.Button Threshold;
+        private System.Windows.Forms.Button imageQuan;
+        private System.Windows.Forms.Button histogram;
+        private System.Windows.Forms.Button imageDifference;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp12/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Digital_Image_Processing
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap originalImage;
+        private Bitmap processedImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Bitmap(openFileDialog.FileName);
+
+                // Display original image
+                pictureBox1.Image = ResizeImageToBox(originalImage, pictureBox1.Width, pictureBox1.Height);
+
+                // Generate histogram for original image
+                GenerateHistogram(originalImage, histogram1);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null) return;
+
+            double mean = CalculateMeanGrayLevel(originalImage);
+            processedImage = ApplyThresholding(originalImage, (byte)mean);
+
+            // Display processed image
+            outputBox.Image = ResizeImageToBox(processedImage, outputBox.Width, outputBox.Height);
+
+            // Generate histogram for processed image
+            GenerateHistogram(processedImage, histogram2);
+        }
+
+        private void imageQuan_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null) return;
+
+            Bitmap quantizedImage8 = ApplyQuantization(originalImage, 8);
+            Bitmap quantizedImage16 = ApplyQuantization(originalImage, 16);
+
+            // Display quantized image (8 levels)
+            outputBox.Image = ResizeImageToBox(quantizedImage8, outputBox.Width, outputBox.Height);
+
+            // Generate histogram for quantized image
+            GenerateHistogram(quantizedImage8, histogram2);
+
+            // Show quantization mapping for both levels
+            ShowQuantizationMapping(8);
+            ShowQuantizationMapping(16);
+
+            // Display comparison popup
+            ShowComparisonPopup(quantizedImage8, "Quantization - 8 Levels", quantizedImage16, "Quantization - 16 Levels");
+        }
+
+        private void histogram_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null) return;
+
+            processedImage = ApplyHistogramEqualization(originalImage);
+
+            // Display processed image
+            outputBox.Image = ResizeImageToBox(processedImage, outputBox.Width, outputBox.Height);
+
+            // Generate histogram for processed image
+            GenerateHistogram(processedImage, histogram2);
+        }
+
+        private void imageDifference_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null || processedImage == null) return;
+
+            Bitmap diffImage = CalculateImageDifference(originalImage, processedImage);
+
+            // Display difference image
+            difBox.Image = ResizeImageToBox(diffImage, difBox.Width, difBox.Height);
+        }
+
+        private void ShowQuantizationMapping(int levels)
+        {
+            Form mappingForm = new Form
+            {
+                Text = $"Quantization Mapping ({levels} Levels)",
+                Size = new Size(600, 400),
+                StartPosition = FormStartPosition.CenterScreen
+            };
+
+            Chart chart = new Chart
+            {
+                Dock = DockStyle.Fill
+            };
+
+            ChartArea chartArea = new ChartArea
+            {
+                AxisX = { Minimum = 0, Maximum = 255, Title = "r (Input Intensity)" },
+                AxisY = { Minimum = 0, Maximum = 255, Title = "s (Quantized Intensity)" }
+            };
+            chart.ChartAreas.Add(chartArea);
+
+            Series series = new Series
+            {
+                ChartType = SeriesChartType.StepLine,
+                Color = Color.Blue,
+                BorderWidth = 2
+            };
+
+            int step = 256 / levels;
+            for (int i = 0; i <= 256; i += step)
+            {
+                int quantizedValue = (i / step) * step + step / 2;
+                series.Points.AddXY(i, quantizedValue);
+                if (i + step < 256)
+                {
+                    series.Points.AddXY(i + step - 1, quantizedValue);
+                }
+            }
+            chart.Series.Add(series);
+
+            mappingForm.Controls.Add(chart);
+            mappingForm.ShowDialog();
+        }
+
+        private void ShowComparisonPopup(Bitmap image1, string title1, Bitmap image2, string title2)
+        {
+            Form popupForm = new Form
+            {
+                Text = "Image Comparison",
+                Size = new Size(800, 400),
+                StartPosition = FormStartPosition.CenterScreen
+            };
+
+            TableLayoutPanel tableLayout = new TableLayoutPanel
+            {
+                RowCount = 2,
+                ColumnCount = 2,
+                Dock = DockStyle.Fill,
+                CellBorderStyle = TableLayoutPanelCellBorderStyle.Single
+            };
+
+            tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
+            tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
+            tableLayout.RowStyles.Add(new RowStyle(SizeType.Absolute, 30)); // Titles
+            tableLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 100)); // Images
+
+            Label label1 = new Label
+            {
+                Text = title1,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Dock = DockStyle.Fill
+            };
+            Label label2 = new Label
+            {
+                Text = title2,
+                TextAlign = ContentAlignment.MiddleCenter,
+                Dock = DockStyle.Fill
+            };
+
+            PictureBox pictureBox1 = new PictureBox
+            {
+                Image = image1,
+                SizeMode = PictureBoxSizeMode.Zoom,
+                Dock = DockStyle.Fill
+            };
+            PictureBox pictureBox2 = new PictureBox
+            {
+                Image = image2,
+                SizeMode = PictureBoxSizeMode.Zoom,
+                Dock = DockStyle.Fill
+            };
+
+            tableLayout.Controls.Add(label1, 0, 0);
+            tableLayout.Controls.Add(label2, 1, 0);
+            tableLayout.Controls.Add(pictureBox1, 0, 1);
+            tableLayout.Controls.Add(pictureBox2, 1, 1);
+
+            popupForm.Controls.Add(tableLayout);
+            popupForm.ShowDialog();
+        }
+
+        private void GenerateHistogram(Bitmap image, PictureBox histogramBox)
+        {
+            int[] histogram = new int[256];
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    int gray = (image.GetPixel(x, y).R + image.GetPixel(x, y).G + image.GetPixel(x, y).B) / 3;
+                    histogram[gray]++;
+                }
+            }
+
+            histogramBox.Controls.Clear();
+
+            Chart chart = new Chart
+            {
+                Dock = DockStyle.Fill
+            };
+
+            ChartArea chartArea = new ChartArea
+            {
+                AxisX = { Minimum = 0, Maximum = 255, Title = "Intensity" },
+                AxisY = { Title = "Frequency" }
+            };
+            chart.ChartAreas.Add(chartArea);
+
+            Series series = new Series
+            {
+                ChartType = SeriesChartType.Column,
+                Color = Color.Blue,
+                Name = "Grayscale Histogram"
+            };
+
+            for (int i = 0; i < 256; i++)
+            {
+                series.Points.AddXY(i, histogram[i]);
+            }
+            chart.Series.Add(series);
+
+            histogramBox.Controls.Add(chart);
+        }
+
+        private Bitmap ResizeImageToBox(Bitmap originalImage, int width, int height)
+        {
+            Bitmap resizedImage = new Bitmap(width, height);
+
+            using (Graphics g = Graphics.FromImage(resizedImage))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.DrawImage(originalImage, 0, 0, width, height);
+            }
+
+            return resizedImage;
+        }
+
+        private double CalculateMeanGrayLevel(Bitmap image)
+        {
+            long totalGray = 0;
+            int pixelCount = image.Width * image.Height;
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Color pixel = image.GetPixel(x, y);
+                    totalGray += (pixel.R + pixel.G + pixel.B) / 3;
+                }
+            }
+
+            return (double)totalGray / pixelCount;
+        }
+
+        private Bitmap ApplyThresholding(Bitmap image, byte threshold)
+        {
+            Bitmap result = new Bitmap(image.Width, image.Height);
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Color pixel = image.GetPixel(x, y);
+                    int gray = (pixel.R + pixel.G + pixel.B) / 3;
+                    int binary = gray >= threshold ? 255 : 0;
+                    result.SetPixel(x, y, Color.FromArgb(binary, binary, binary));
+                }
+            }
+
+            return result;
+        }
+
+        private Bitmap ApplyQuantization(Bitmap image, int levels)
+        {
+            Bitmap result = new Bitmap(image.Width, image.Height);
+            int step = 256 / levels;
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Color pixel = image.GetPixel(x, y);
+                    int gray = (pixel.R + pixel.G + pixel.B) / 3;
+                    int quantized = (gray / step) * step + step / 2;
+                    result.SetPixel(x, y, Color.FromArgb(quantized, quantized, quantized));
+                }
+            }
+
+            return result;
+        }
+
+        private Bitmap ApplyHistogramEqualization(Bitmap image)
+        {
+            Bitmap result = new Bitmap(image.Width, image.Height);
+            int[] histogram = new int[256];
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    int gray = (image.GetPixel(x, y).R + image.GetPixel(x, y).G + image.GetPixel(x, y).B) / 3;
+                    histogram[gray]++;
+                }
+            }
+
+            int[] cdf = new int[256];
+            cdf[0] = histogram[0];
+            for (int i = 1; i < 256; i++)
+            {
+                cdf[i] = cdf[i - 1] + histogram[i];
+            }
+
+            int totalPixels = image.Width * image.Height;
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    int gray = (image.GetPixel(x, y).R + image.GetPixel(x, y).G + image.GetPixel(x, y).B) / 3;
+                    int equalized = cdf[gray] * 255 / totalPixels;
+                    result.SetPixel(x, y, Color.FromArgb(equalized, equalized, equalized));
+                }
+            }
+
+            return result;
+        }
+
+        private Bitmap CalculateImageDifference(Bitmap original, Bitmap processed)
+        {
+            Bitmap result = new Bitmap(original.Width, original.Height);
+
+            for (int y = 0; y < original.Height; y++)
+            {
+                for (int x = 0; x < original.Width; x++)
+                {
+                    Color origPixel = original.GetPixel(x, y);
+                    Color procPixel = processed.GetPixel(x, y);
+
+                    int diffR = Math.Abs(origPixel.R - procPixel.R);
+                    int diffG = Math.Abs(origPixel.G - procPixel.G);
+                    int diffB = Math.Abs(origPixel.B - procPixel.B);
+
+                    result.SetPixel(x, y, Color.FromArgb(diffR, diffG, diffB));
+                }
+            }
+
+            return result;
+        }
+    }
+}
